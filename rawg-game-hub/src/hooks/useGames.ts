781ffc3328ebdae08718c8fe0559f93d7d0605a2
Ruchi Agrawal <ruchi.ggc@gmail.com>
2023-12-04 12:49:39 +0530

    last functional rawg code

---
+++ rawg-game-hub/src/hooks/useGames.ts
@@ -0,0 +1,31 @@
+
+import { useState, useEffect } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+export interface Game {
+    id: number;
+    name: string;
+    background_image : string;
+  }
+  interface FetchGamesResponse {
+    count: number;
+    results: Game[];
+  }
+const useGames = () =>{
+    const [games, setGames] = useState<Game[]>([]);
+    const [error, setError] = useState("");
+    useEffect(() => {
+        const abortController = new AbortController();
+      apiClient
+        .get<FetchGamesResponse>("./movie", {signal: abortController.signal})
+        .then((res) => setGames(res.data.results))
+        .catch((err) => {
+            if(err instanceof CanceledError)return;
+            setError(err.message)
+        });
+        return abortController.abort();
+    }, []);
+    return{games, error};
+}
+
+export default useGames;
