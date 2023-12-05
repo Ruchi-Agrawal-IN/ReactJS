2cd41840dead961fa79347223f580c958c2d34b8
Ruchi Agrawal <ruchi.ggc@gmail.com>
2023-12-05 12:48:15 +0530

    croped downloading image on fly

--- rawg-game-hub/src/components/GameCard.tsx
+++ rawg-game-hub/src/components/GameCard.tsx
@@ -2,13 +2,14 @@ import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIocnList from "./PlatformIocnList";
 import CriticScore from "./CriticScore";
+import getCroppedImageurl from "../services/image-url";
 interface Props {
   game: Game;
 }
 const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow={"hidden"}>
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageurl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
