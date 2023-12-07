06a9fdbf723598f0ba4d2dcabd2db6a69f6d9272
Ruchi Agrawal <ruchi.ggc@gmail.com>
2023-12-07 13:40:21 +0530

    handling Games without image

--- rawg-game-hub/src/services/image-url.ts
+++ rawg-game-hub/src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from '../assets/GameHub Resources/Image Placeholder/no-image-placeholder.webp'
+
 const getCroppedImageurl = (url: string, width: number, height: number) => {
-    if(!url) return '';
+    if(!url) return noImage;
     const target = 'media/';
     const index = url.indexOf(target)+ target.length;
     const addedStr = 'crop/'+height+'/'+width+'/';
