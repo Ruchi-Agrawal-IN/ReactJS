5e00a5e7d98935ae51570cabe097fca06d9dc99f
Ruchi Agrawal <ruchi.ggc@gmail.com>
2023-12-07 23:45:04 +0530

    refactoring gamegrid

--- rawg-game-hub/src/components/GameGrid.tsx
+++ rawg-game-hub/src/components/GameGrid.tsx
@@ -11,27 +11,25 @@ interface Props {
 const GameGrid = ({ gameQry }: Props) => {
   const { data, error, isLoading } = useGames(gameQry);
   const skeletons = [1, 2, 3, 4, 5, 6];
+  if (error) return <Text>{error}</Text>;
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        padding="10px"
-        spacing={6}
-      >
-        {isLoading &&
-          skeletons.map((skltn) => (
-            <GameCardContainer key={skltn}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        {data.map((g) => (
-          <GameCardContainer key={g.id}>
-            <GameCard game={g} />
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      padding="10px"
+      spacing={6}
+    >
+      {isLoading &&
+        skeletons.map((skltn) => (
+          <GameCardContainer key={skltn}>
+            <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map((g) => (
+        <GameCardContainer key={g.id}>
+          <GameCard game={g} />
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
